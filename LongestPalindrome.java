0fff9e2ae31457cde7aed0c3dd6f9ab49027cfad
doradee <wudi99@gmail.com>
2014-12-09 12:52:20 -0500

    fix the bug and got the solution accepted.

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -6,20 +6,20 @@ package solutions;
  * and there exists one unique longest palindromic substring.
  */
 public class LongestPalindrome {
-  
-  public static String find_LPS(String input) {
-    if (input.isEmpty()) {
+  /* this solution is wrong because it only works for odd length substrings */
+  public static String find_LPS1(String s) {
+    if (s.isEmpty()) {
       return "";
     }
     int count = 0;
     int max = -1;
     String result = "";
-    for (int i = 0; i < input.length(); i++) {
-      while ((i - count) >= 0 && (i + count) < input.length()
-          && (input.charAt(i - count) == input.charAt(i + count))) {
+    for (int i = 0; i < s.length(); i++) {
+      while ((i - count) >= 0 && (i + count) < s.length()
+          && (s.charAt(i - count) == s.charAt(i + count))) {
         if ((2 * count + 1) > max) {
           max = 2 * count + 1;
-          result = input.substring(i - count, i + count + 1);
+          result = s.substring(i - count, i + count + 1);
         }
         count++;
       }
@@ -28,8 +28,32 @@ public class LongestPalindrome {
     return result;
   }
   
+  /* this is a dynamic programming approach */
+  /* This is the accepted answer */
+  public static String find_LPS2(String s) {
+    String result = s.substring(0, 1);
+    if (s.isEmpty()) {
+      return result;
+    }
+    for (int i = 1; i < s.length(); i++) {
+      String lp1 = lps(s, i-1, i);
+      String lp2 = lps(s, i-1, i+1);
+      String longer = lp1.length() > lp2.length()? lp1: lp2;
+      result = longer.length() > result.length()? longer: result;
+    }
+    return result;
+  }
+  
+  public static String lps(String s, int left, int right) {
+    while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+      left--;
+      right++;
+    }
+    return s.substring(left + 1, right);
+  }
+  
   public static void main(String[] args) {
-    String input = "abcdcccccccccccdddddddddc";
-    System.out.println(find_LPS(input));
+    String input = "a";
+    System.out.println(find_LPS2(input));
   }
 }
