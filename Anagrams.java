e58feffd4fdc952302686978101250ead0f9e566
doradee <wudi99@gmail.com>
2014-12-11 11:14:37 -0500

    updated solution for anagrams - switch key and value for a hashtable did the trick

--- Anagrams.java
+++ Anagrams.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
  */
 public class Anagrams {
   /* TLE */
-  public static ArrayList<String> anagrams(String[] strs) {
+  public static ArrayList<String> anagrams1(String[] strs) {
     ArrayList<String> result = new ArrayList<String>();
     if (strs.length <= 1) {
       return result;
@@ -39,4 +39,47 @@ public class Anagrams {
     }
     return result;
   }
+  
+  /* Accetped Solution. Inspired by discussion based on the problem */
+  public static ArrayList<String> anagrams(String[] strs) {
+    ArrayList<String> result = new ArrayList<String>();
+    HashMap<String, ArrayList<Integer>> dic = new HashMap<String, ArrayList<Integer>>();
+    if (strs.length <= 1) {
+      return result;
+    }
+    for (int i = 0; i < strs.length; i++) {
+      String key = sort(strs[i]);
+      if (dic.containsKey(key)) {
+        dic.get(key).add(i);
+      } else {
+        ArrayList<Integer> index = new ArrayList<Integer>();
+        index.add(i);
+        dic.put(key, index);
+      }
+    }
+    for (String key : dic.keySet()) {
+      if (dic.get(key).size() > 1) {
+        for (int j: dic.get(key)) {
+          result.add(strs[j]);
+        }
+      }
+    }
+    
+    return result;
+  }
+  
+  public static String sort(String str) {
+    char[] chars = str.toCharArray();
+    Arrays.sort(chars);
+    return Arrays.toString(chars);
+  }
+  
+  public static void main(String[] args) {
+    String[] strs = {"abbb", "babb", "bcbb", "bbab"};
+    System.out.println(anagrams(strs).toString());
+    for (String s : strs) {
+      System.out.println(sort(s).toString());
+    }
+  }
+  
 }
