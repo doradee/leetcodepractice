f051b0cb9a19190e74a311694be1f98f835d7d8b
doradee <wudi99@gmail.com>
2014-12-13 15:48:51 -0500

    o(lgn) is a little bit complex. But good to know.

--- FindMinimumInRotatedSortedArray.java
+++ FindMinimumInRotatedSortedArray.java
@@ -8,19 +8,6 @@ package solutions;
  */
 public class FindMinimumInRotatedSortedArray {
   
-  /* o(n) solution. Trivial */
-  public static int findMin_ON(int[] num) {
-    if (num == null || num.length == 0) {
-      return 0;
-    }
-    for (int i = 0; i < num.length - 1; i++) {
-      if (num[i] > num[i + 1]) {
-        return num[i + 1];
-      }
-    }
-    return num[0];
-  }
-  
   /* o(lgn) solution. */
   public static int findMin(int[] num) {
     if (num == null || num.length == 0) {
@@ -42,5 +29,18 @@ public class FindMinimumInRotatedSortedArray {
       }
     }
     return num[right + 1];
+  }
+  
+  /* o(n) solution. Trivial */
+  public static int findMin_ON(int[] num) {
+    if (num == null || num.length == 0) {
+      return 0;
+    }
+    for (int i = 0; i < num.length - 1; i++) {
+      if (num[i] > num[i + 1]) {
+        return num[i + 1];
+      }
+    }
+    return num[0];
   }
 }
