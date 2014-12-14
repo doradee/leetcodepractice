b44ded7edab0bc4e399608edee28181fb324b43e
doradee <wudi99@gmail.com>
2014-12-14 11:04:32 -0500

    9Z's solution added. Space is o(n^2) though.

--- Triangle.java
+++ Triangle.java
@@ -21,8 +21,9 @@ import java.util.ArrayList;
  */
 public class Triangle {
   
-  /* Accepted solution. The idea is all right. But stuck in indexes of pre and cur.
-   * be sure to clear about size of list, and index of elements.
+  /*
+   * Accepted solution. The idea is all right. But stuck in indexes of pre and
+   * cur. be sure to clear about size of list, and index of elements.
    */
   public static int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
     if (triangle == null || triangle.size() == 0) {
@@ -37,9 +38,8 @@ public class Triangle {
       ArrayList<Integer> current = new ArrayList<Integer>();
       current.add(pre.get(0) + triangle.get(i).get(0));
       for (int j = 0; j < pre.size() - 1; j++) {
-        
         current.add(Math.min(pre.get(j + 1), pre.get(j))
-            + triangle.get(i).get(j+1));
+            + triangle.get(i).get(j + 1));
       }
       current.add(pre.get(pre.size() - 1)
           + triangle.get(i).get(triangle.get(i).size() - 1));
@@ -52,6 +52,25 @@ public class Triangle {
       }
     }
     return minTotal;
+  }
+  
+  /* 9Z's solution. Easier to understand. But space complexity is o(n^2) */
+  public int minimumTotal_9Z(ArrayList<ArrayList<Integer>> triangle) {
+    if (triangle == null || triangle.size() == 0) {
+      return 0;
+    }
+    int n = triangle.size();
+    int[][] sum = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      sum[n - 1][i] = triangle.get(n - 1).get(i);
+    }
+    for (int i = n - 2; i >= 0; i--) {
+      for (int j = 0; j <= i; j++) {
+        sum[i][j] = Math.min(sum[i + 1][j], sum[i + 1][j + 1])
+            + triangle.get(i).get(j);
+      }
+    }
+    return sum[0][0];
   }
   
   public static void main(String[] args) {
