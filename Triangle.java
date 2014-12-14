3f666bdc255940167eacd9365a365bcf91d608e0
doradee <wudi99@gmail.com>
2014-12-14 00:45:31 -0500

    solution is accepted. made too many mistakes on indexing.

---
+++ Triangle.java
@@ -0,0 +1,73 @@
+package solutions;
+
+import java.util.ArrayList;
+
+/*
+ Given a triangle, find the minimum path sum from top to bottom. 
+ Each step you may move to adjacent numbers on the row below.
+
+ For example, given the following triangle
+ [
+ [2],
+ [3,4],
+ [6,5,7],
+ [4,1,8,3]
+ ]
+ The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+ Note:
+ Bonus point if you are able to do this using only O(n) extra space, 
+ where n is the total number of rows in the triangle.
+ */
+public class Triangle {
+  
+  /* Accepted solution. The idea is all right. But stuck in indexes of pre and cur.
+   * be sure to clear about size of list, and index of elements.
+   */
+  public static int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+    if (triangle == null || triangle.size() == 0) {
+      return 0;
+    }
+    ArrayList<Integer> pre = new ArrayList<Integer>();
+    pre.add(triangle.get(0).get(0));
+    if (triangle.size() == 1) {
+      return triangle.get(0).get(0);
+    }
+    for (int i = 1; i < triangle.size(); i++) {
+      ArrayList<Integer> current = new ArrayList<Integer>();
+      current.add(pre.get(0) + triangle.get(i).get(0));
+      for (int j = 0; j < pre.size() - 1; j++) {
+        
+        current.add(Math.min(pre.get(j + 1), pre.get(j))
+            + triangle.get(i).get(j+1));
+      }
+      current.add(pre.get(pre.size() - 1)
+          + triangle.get(i).get(triangle.get(i).size() - 1));
+      pre = current;
+    }
+    int minTotal = Integer.MAX_VALUE;
+    for (int i : pre) {
+      if (i < minTotal) {
+        minTotal = i;
+      }
+    }
+    return minTotal;
+  }
+  
+  public static void main(String[] args) {
+    ArrayList<ArrayList<Integer>> triangle = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> l1 = new ArrayList<Integer>();
+    l1.add(-1);
+    ArrayList<Integer> l2 = new ArrayList<Integer>();
+    l2.add(2);
+    l2.add(3);
+    ArrayList<Integer> l3 = new ArrayList<Integer>();
+    l3.add(1);
+    l3.add(-1);
+    l3.add(-1);
+    triangle.add(l1);
+    triangle.add(l2);
+    triangle.add(l3);
+    System.out.println(minimumTotal(triangle));
+  }
+}
