802b4b93d405b65d71d7c7c6b7cd1284c6e18765
doradee <wudi99@gmail.com>
2014-12-13 21:28:03 -0500

    recursive solution. Trivial.

---
+++ SumRoottoLeafNumbers.java
@@ -0,0 +1,45 @@
+package solutions;
+
+/*
+ Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+ An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+ Find the total sum of all root-to-leaf numbers.
+
+ For example,
+
+ 1
+ / \
+ 2   3
+ The root-to-leaf path 1->2 represents the number 12.
+ The root-to-leaf path 1->3 represents the number 13.
+
+ Return the sum = 12 + 13 = 25.
+ */
+public class SumRoottoLeafNumbers {
+  
+  /* Accepted */
+  public static int sumNumbers(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int sum = 0;
+    return sumNumbersCal(root, sum);
+  }
+  
+  public static int sumNumbersCal(TreeNode root, int sum) {
+    if (root.left == null && root.right == null) {
+      return sum * 10 + root.val;
+    } else if (root.left != null && root.right == null) {
+      sum = sum * 10 + root.val;
+      return sumNumbersCal(root.left, sum);
+    } else if (root.left == null && root.right != null) {
+      sum = sum * 10 + root.val;
+      return sumNumbersCal(root.right, sum);
+    } else {
+      sum = sum * 10 + root.val;
+      return sumNumbersCal(root.left, sum) + sumNumbersCal(root.right, sum);
+    }
+  }
+}
