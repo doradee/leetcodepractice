d8221c035dac000eb61113e5dbc59434d4fe4c62
doradee <wudi99@gmail.com>
2014-12-18 00:04:11 -0500

    9Z's DP solution added.

--- RestoreIPAddresses.java
+++ RestoreIPAddresses.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
  */
 public class RestoreIPAddresses {
+  
   /* Learned from discussion. Very nice non-DP solution */
   public ArrayList<String> restoreIpAddresses(String s) {
     ArrayList<String> res = new ArrayList<String>();
@@ -38,5 +39,46 @@ public class RestoreIPAddresses {
       return false;
     }
     return true;
+  }
+  
+  /* You may want to see 9Z's solution which is DP */
+  public ArrayList<String> restoreIpAddresses_9Z(String s) {
+    ArrayList<String> result = new ArrayList<String>();
+    ArrayList<String> list = new ArrayList<String>();
+    if (s.length() < 4 || s.length() > 12)
+      return result;
+    helper(result, list, s, 0);
+    return result;
+  }
+  
+  public void helper(ArrayList<String> result, ArrayList<String> list,
+      String s, int start) {
+    if (list.size() == 4) {
+      if (start != s.length())
+        return;
+      StringBuffer sb = new StringBuffer();
+      for (String tmp : list) {
+        sb.append(tmp);
+        sb.append(".");
+      }
+      sb.deleteCharAt(sb.length() - 1);
+      result.add(sb.toString());
+      return;
+    }
+    for (int i = start; i < s.length() && i <= start + 3; i++) {
+      String tmp = s.substring(start, i + 1);
+      if (isvalid_9Z(tmp)) {
+        list.add(tmp);
+        helper(result, list, s, i + 1);
+        list.remove(list.size() - 1);
+      }
+    }
+  }
+  
+  private boolean isvalid_9Z(String s) {
+    if (s.charAt(0) == '0')
+      return s.equals("0"); // to eliminate cases like "00", "10"
+    int digit = Integer.valueOf(s);
+    return digit >= 0 && digit <= 255;
   }
 }
